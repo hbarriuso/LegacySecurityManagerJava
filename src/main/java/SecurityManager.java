0237b3d3d190858af47a80383edb90e3328e6e96
Héctor Hugo Barriuso Mata <hbarriuso@gmail.com>
2016-11-28 21:01:42 +0100

    Added files

--- src/main/java/SecurityManager.java
+++ src/main/java/SecurityManager.java
@@ -2,47 +2,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class SecurityManager {
+    private final BufferedReader in;
+    private final PrintStream out;
+    private final PrintStream err;
+
+    public SecurityManager(BufferedReader in, PrintStream out, PrintStream err) {
+        this.in = in;
+        this.out = out;
+        this.err = err;
+    }
+
     public static void createUser() {
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        final PrintStream out = System.out;
+        final PrintStream err = System.err;
+        new SecurityManager(in, out, err).innerCreateUser();
+    }
+
+    public void innerCreateUser() {
 
         String username = null;
         String fullName = null;
         String password = null;
         String confirmPassword = null;
         try {
-            System.out.println("Enter a username");
-            username = buffer.readLine();
-            System.out.println("Enter your full name");
-            fullName = buffer.readLine();
-            System.out.println("Enter your password");
-            password = buffer.readLine();
-            System.out.println("Re-enter your password");
-            confirmPassword = buffer.readLine();
+            writeToConsole("Enter a username");
+            username = readLine();
+            writeToConsole("Enter your full name");
+            fullName = readLine();
+            writeToConsole("Enter your password");
+            password = readLine();
+            writeToConsole("Re-enter your password");
+            confirmPassword = readLine();
         } catch (IOException e) {
-            e.printStackTrace();
+            printIOException(e);
         }
 
         if (!password.equals(confirmPassword)) {
-            System.out.println("The passwords don't match");
+            writeToConsole("The passwords don't match");
             return;
         }
 
         if (password.length() < 8) {
-            System.out.println("Password must be at least 8 characters in length");
+            writeToConsole("Password must be at least 8 characters in length");
             return;
         }
 
         // Encrypt the password (just reverse it, should be secure)
         String encryptedPassword = new StringBuilder(password).reverse().toString();
 
-        System.out.println(
-            String.format(
-            "Saving Details for User (%s, %s, %s)\n",
-            username,
-            fullName,
-            encryptedPassword)
-        );
+        writeToConsole(String.format(
+                "Saving Details for User (%s, %s, %s)\n",
+                username,
+                fullName,
+                encryptedPassword));
+    }
+
+    private String readLine() throws IOException {
+        return in.readLine();
+    }
+
+    private void writeToConsole(String line) {
+        out.println(line);
+    }
+
+    private void printIOException(IOException e) {
+        e.printStackTrace(err);
     }
 }
--- src/test/java/SecurityManagerTest.java
+++ src/test/java/SecurityManagerTest.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 
 public class SecurityManagerTest {
+    private BufferedReader buffer;
+    private PrintStream out;
+    private PrintStream err;
+    private SecurityManager securityManager;
+
+    @BeforeMethod
+    public void setUp() {
+        buffer = mock(BufferedReader.class);
+        out = mock(PrintStream.class);
+        err = mock(PrintStream.class);
+        securityManager = new SecurityManager(buffer, out, err);
+    }
+
+    @Test
+    public void testEverythingIsOk() throws IOException {
+        when(buffer.readLine()).thenReturn("Pepe", "Pepe Sanchez", "12345678");
+
+        securityManager.innerCreateUser();
+
+        verify(out).println("Saving Details for User (Pepe, Pepe Sanchez, 87654321)\n");
+    }
+
+    @Test
+    public void testPasswordsDontMatch() throws IOException {
+        when(buffer.readLine()).thenReturn("Pepe", "Pepe Sanchez", "12345678", "12345679");
+
+        securityManager.innerCreateUser();
+
+        verify(out).println("The passwords don't match");
+    }
+
+    @Test
+    public void testPasswordMustBeAtLeastEightCharacters() throws IOException {
+        when(buffer.readLine()).thenReturn("Pepe", "Pepe Sanchez", "123456", "123456");
+
+        securityManager.innerCreateUser();
+
+        verify(out).println("Password must be at least 8 characters in length");
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testNullPointerExceptionIsThrownWhenAnIOExceptionIsThrownBeforePasswordIsGiven() throws IOException {
+        when(buffer.readLine()).thenThrow(new IOException("Fail!"));
+        securityManager.innerCreateUser();
+    }
+
+    @Test
+    public void testIOExceptionIsCatched() throws IOException {
+        IOException expected = new IOException("Fail!");
+        when(buffer.readLine()).thenReturn("Pepe", "Pepe Sanchez", "12345678").thenThrow(expected);
+        securityManager.innerCreateUser();
+        verify(err).println(expected);
+    }
 }
